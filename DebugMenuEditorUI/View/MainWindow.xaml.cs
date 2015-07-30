ab90bc02904f11be3a5f79d6267bbb809b488620
Matt Hoffman <matt@luxeloperation.com>
2015-07-30 00:57:15 -0700

    Initial check-in of basic UI mockup following MVVM patterns.

---
+++ DebugMenuEditorUI/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DebugMenuEditorUI.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
